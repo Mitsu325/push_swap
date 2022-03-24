f52b980b111b20f6c3594e9bb45777a1dbcd85ea
Mitsu325 <patricia.mashiba@gmail.com>
2022-03-25 00:16:26 +0100

    feat: ensure entry of value

---
+++ src/main.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/24 23:59:16 by pmitsuko          #+#    #+#             */
+/*   Updated: 2022/03/25 00:11:58 by pmitsuko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+#include "stdio.h"
+
+int	main(int argc, char **argv)
+{
+	if (argc < 2)
+		return (EXIT_FAILURE);
+	printf("%s", argv[0]);
+	return (EXIT_SUCCESS);
+}
