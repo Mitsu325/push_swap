54170fb359fd749bf85b369557c36aba4bee1c3f
Mitsu325 <patricia.mashiba@gmail.com>
2022-03-25 01:14:04 +0100

    refactor: norminette

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/24 23:59:16 by pmitsuko          #+#    #+#             */
-/*   Updated: 2022/03/25 01:08:05 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2022/03/25 01:13:44 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,6 @@
 
 int	main(int argc, char **argv)
 {
-	// Test!!
 	if (argc < 2)
 		return (EXIT_FAILURE);
 	printf("%s", argv[0]);
