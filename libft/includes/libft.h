8bbf3e484e093da3d1c466ddd7ea6c15cbb82029
Mitsu325 <patricia.mashiba@gmail.com>
2022-03-31 02:59:55 +0200

    feat: create ft_atoi in libft

--- libft/includes/libft.h
+++ libft/includes/libft.h
@@ -6,13 +6,18 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 01:27:18 by pmitsuko          #+#    #+#             */
-/*   Updated: 2022/03/26 01:28:13 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2022/03/31 02:29:00 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
+# define SPC 0x020
+# define TAB 0x009
+# define CR 0x00D
+
 int	ft_isdigit(int c);
+int	ft_atoi(const char *str);
 
 #endif
---
+++ libft/src/num/ft_atoi.c
@@ -0,0 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/31 02:21:00 by pmitsuko          #+#    #+#             */
+/*   Updated: 2022/03/31 02:57:44 by pmitsuko         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	is_white_space(int c, int is_first_sign)
+{
+	if ((c == SPC || (c >= TAB && c <= CR)) && is_first_sign)
+		return (1);
+	return (0);
+}
+
+int	calculate_num(const char *str, int sign)
+{
+	int num;
+
+	num = 0;
+	while (ft_isdigit(*str))
+	{
+		num = num * 10 + (*str - 48);
+		str++;
+	}
+	return (sign * num);
+}
+
+int	ft_atoi(const char *str)
+{
+	int	sign;
+	int	is_first_sign;
+
+	sign = 1;
+	is_first_sign = 1;
+	while (*str != '\0')
+	{
+		if (*str == '-' && is_first_sign)
+		{
+			sign = -1;
+			is_first_sign = 0;
+		}
+		else if (*str == '+' && is_first_sign)
+			is_first_sign = 0;
+		else if (ft_isdigit(*str))
+			return (calculate_num(str, sign));
+		else if (!is_white_space(*str, is_first_sign))
+			return (0);
+		str++;
+	}
+	return (0);
+}
