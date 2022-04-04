16bec327034381256d22cc0c98251fc6382c803f
Mitsu325 <patricia.mashiba@gmail.com>
2022-04-04 05:34:14 -0300

    test: atoi tester

--- libft/tests/num/atoi_test.c
+++ libft/tests/num/atoi_test.c
@@ -1,8 +1,66 @@
 #include <criterion/criterion.h>
+#include "../../includes/libft.h"
 
-Test(atoi, simple) {
-	int	num;
+Test(atoi, positive_num) {
+	const char	*str = "127";
+	int	my_num = ft_atoi(str);
+	int	num_expect = atoi(str);
 
-	num = 5;
-	cr_expect(num == 5, "Tester OK!!");
+	cr_expect(my_num == num_expect, "Ensure returns a positive number");
+}
+
+Test(atoi, negative_num) {
+	const char	*str = "-127";
+	int	my_num = ft_atoi(str);
+	int	num_expect = atoi(str);
+
+	cr_expect(my_num == num_expect, "Ensure returns a negative number");
+}
+
+Test(atoi, integer_max) {
+	const char	*str = "+2147483647";
+	int	my_num = ft_atoi(str);
+	int	num_expect = atoi(str);
+
+	cr_expect(my_num == num_expect, "Ensure returns a integer max number");
+}
+
+Test(atoi, integer_min) {
+	const char	*str = "-2147483648";
+	int	my_num = ft_atoi(str);
+	int	num_expect = atoi(str);
+
+	cr_expect(my_num == num_expect, "Ensure returns a min integer");
+}
+
+Test(atoi, greater_integer_max) {
+	const char	*str = "+2147483650";
+	int	my_num = ft_atoi(str);
+	int	num_expect = atoi(str);
+
+	cr_expect(my_num == num_expect, "Ensure returns greater than the max integer");
+}
+
+Test(atoi, less_integer_min) {
+	const char	*str = "-2147483650";
+	int	my_num = ft_atoi(str);
+	int	num_expect = atoi(str);
+
+	cr_expect(my_num == num_expect, "Ensure returns less than the min integer");
+}
+
+Test(atoi, signal) {
+	const char	*str = "127+-098";
+	int	my_num = ft_atoi(str);
+	int	num_expect = atoi(str);
+
+	cr_expect(my_num == num_expect, "Ensure consecutive signal handling");
+}
+
+Test(atoi, negative_signal) {
+	const char	*str = "--127098";
+	int	my_num = ft_atoi(str);
+	int	num_expect = atoi(str);
+
+	cr_expect(my_num == num_expect, "Ensure consecutive negative signal handling");
 }
