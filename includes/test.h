69b68f6f86e9137b4fb8ee7d4f024a13b06de43e
Mitsu325 <patricia.mashiba@gmail.com>
2022-04-06 07:13:02 -0300

    refactor: change test name

--- includes/test.h
+++ includes/test.h
@@ -6,7 +6,7 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 05:24:28 by pmitsuko          #+#    #+#             */
-/*   Updated: 2022/04/06 05:25:47 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2022/04/06 07:11:41 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,6 @@
 # include "push_swap.h"
 
 int	push_swap_test(void);
-int	check_is_integer(void);
+int	check_is_digit(void);
 
 #endif
--- src/tests/check_list_integer.c
+++ src/tests/check_list_integer.c
@@ -6,13 +6,13 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 05:24:10 by pmitsuko          #+#    #+#             */
-/*   Updated: 2022/04/06 06:30:37 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2022/04/06 07:11:36 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "test.h"
 
-int	check_is_integer(void)
+int	check_is_digit(void)
 {
 	char	*list[3];
 	int		result;
@@ -23,11 +23,11 @@ int	check_is_integer(void)
 	result = check_list_integer(list);
 	if (result == EXIT_SUCCESS)
 	{
-		printf("[check_list_integer_test.c, check_is_integer]");
+		printf("[check_list_integer_test.c, check_is_digit]");
 		printf("- OK\n");
 		return (EXIT_SUCCESS);
 	}
-	printf("[check_list_integer_test.c, check_is_integer]");
+	printf("[check_list_integer_test.c, check_is_digit]");
 	printf("- Ensure returns 0 if list is integer\n");
 	return (EXIT_FAILURE);
 }
