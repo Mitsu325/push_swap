400c7a3d2f9620005f8ec429e38cf152b522eabb
Mitsu325 <patricia.mashiba@gmail.com>
2022-04-06 15:42:21 -0300

    refactor: test

--- includes/test.h
+++ includes/test.h
@@ -6,7 +6,7 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 05:24:28 by pmitsuko          #+#    #+#             */
-/*   Updated: 2022/04/06 08:47:28 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2022/04/06 15:41:21 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,12 @@
 # include <stdio.h>
 # include "push_swap.h"
 
+typedef struct s_check_list_int
+{
+	char	*list[3];
+	int		result;
+}		t_check_list_int;
+
 int	push_swap_test(void);
 
 int	check_isdigit(void);
@@ -24,5 +30,7 @@ int	check_isnumber(void);
 int	check_is_not_number(void);
 int	check_num_with_space(void);
 int	check_not_num_with_space(void);
+
+int	print_status(char *filename, char *fname, char *message, int status);
 
 #endif
--- src/tests/check_list_integer.c
+++ src/tests/check_list_integer.c
@@ -6,7 +6,7 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 05:24:10 by pmitsuko          #+#    #+#             */
-/*   Updated: 2022/04/06 08:48:48 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2022/04/06 15:40:58 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,120 +14,104 @@
 
 int	check_isdigit(void)
 {
-	char	*list[3];
-	int		result;
+	t_check_list_int	check;
 
-	list[0] = "0";
-	list[1] = "2";
-	list[2] = "\0";
-	result = check_list_integer(list);
-	if (result == EXIT_SUCCESS)
+	check.list[0] = "0";
+	check.list[1] = "2";
+	check.list[2] = "\0";
+	check.result = check_list_integer(check.list);
+	if (check.result == EXIT_SUCCESS)
 	{
-		printf("[check_list_integer_test.c, check_isdigit]");
-		printf("- OK\n");
-		return (EXIT_SUCCESS);
+		return (print_status("check_list_integer_test.c", "check_isdigit", "OK",
+		EXIT_SUCCESS));
 	}
-	printf("[check_list_integer_test.c, check_isdigit]");
-	printf("- Ensure returns 0 if list is integer\n");
-	return (EXIT_FAILURE);
+	return (print_status("check_list_integer_test.c", "check_isdigit",
+	"Ensure returns 0 if list is integer", EXIT_FAILURE));
 }
 
 int	check_is_not_digit(void)
 {
-	char	*list[3];
-	int		result;
+	t_check_list_int	check;
 
-	list[0] = "0";
-	list[1] = "A";
-	list[2] = "\0";
-	result = check_list_integer(list);
-	if (result == EXIT_FAILURE)
+	check.list[0] = "0";
+	check.list[1] = "A";
+	check.list[2] = "\0";
+	check.result = check_list_integer(check.list);
+	if (check.result == EXIT_FAILURE)
 	{
-		printf("[check_list_integer_test.c, check_is_not_digit]");
-		printf("- OK\n");
-		return (EXIT_SUCCESS);
+		return (print_status("check_list_integer_test.c", "check_is_not_digit",
+		"OK", EXIT_SUCCESS));
 	}
-	printf("[check_list_integer_test.c, check_is_not_digit]");
-	printf("- Ensure returns 1 if list containst other non-digit char\n");
-	return (EXIT_FAILURE);
+	return (print_status("check_list_integer_test.c", "check_is_not_digit",
+	"Ensure returns 1 if list containst other non-digit char", EXIT_FAILURE));
 }
 
 int	check_isnumber(void)
 {
-	char	*list[3];
-	int		result;
+	t_check_list_int	check;
 
-	list[0] = "10";
-	list[1] = "202";
-	list[2] = "\0";
-	result = check_list_integer(list);
-	if (result == EXIT_SUCCESS)
+	check.list[0] = "10";
+	check.list[1] = "202";
+	check.list[2] = "\0";
+	check.result = check_list_integer(check.list);
+	if (check.result == EXIT_SUCCESS)
 	{
-		printf("[check_list_integer_test.c, check_isnumber]");
-		printf("- OK\n");
-		return (EXIT_SUCCESS);
+		return (print_status("check_list_integer_test.c", "check_isnumber",
+		"OK", EXIT_SUCCESS));
 	}
-	printf("[check_list_integer_test.c, check_isnumber]");
-	printf("- Ensure returns 0 if is number\n");
-	return (EXIT_FAILURE);
+	return (print_status("check_list_integer_test.c", "check_isnumber",
+	"Ensure returns 0 if is number", EXIT_FAILURE));
 }
 
 int	check_is_not_number(void)
 {
-	char	*list[3];
-	int		result;
+	t_check_list_int	check;
 
-	list[0] = "10";
-	list[1] = "Hey";
-	list[2] = "\0";
-	result = check_list_integer(list);
-	if (result == EXIT_FAILURE)
+	check.list[0] = "10";
+	check.list[1] = "Hey";
+	check.list[2] = "\0";
+	check.result = check_list_integer(check.list);
+	if (check.result == EXIT_FAILURE)
 	{
-		printf("[check_list_integer_test.c, check_is_not_number]");
-		printf("- OK\n");
-		return (EXIT_SUCCESS);
+		return (print_status("check_list_integer_test.c", "check_is_not_number",
+		"OK", EXIT_SUCCESS));
 	}
-	printf("[check_list_integer_test.c, check_is_not_number]");
-	printf("- Ensure returns 1 if if list containst other non-number char\n");
-	return (EXIT_FAILURE);
+	return (print_status("check_list_integer_test.c", "check_is_not_number",
+	"Ensure returns 1 if if list containst other non-number char",
+	EXIT_FAILURE));
 }
 
 int	check_num_with_space(void)
 {
-	char	*list[3];
-	int		result;
+	t_check_list_int	check;
 
-	list[0] = "012  1";
-	list[1] = "2  	70	80";
-	list[2] = "\0";
-	result = check_list_integer(list);
-	if (result == EXIT_SUCCESS)
+	check.list[0] = "012  1";
+	check.list[1] = "2  	70	80";
+	check.list[2] = "\0";
+	check.result = check_list_integer(check.list);
+	if (check.result == EXIT_SUCCESS)
 	{
-		printf("[check_list_integer_test.c, check_num_with_space]");
-		printf("- OK\n");
-		return (EXIT_SUCCESS);
+		return (print_status("check_list_integer_test.c",
+		"check_num_with_space", "OK", EXIT_SUCCESS));
 	}
-	printf("[check_list_integer_test.c, check_num_with_space]");
-	printf("- Ensure returns 0 if number in format string with spaces\n");
-	return (EXIT_FAILURE);
+	return (print_status("check_list_integer_test.c", "check_num_with_space",
+	"Ensure returns 0 if number in format string with spaces", EXIT_FAILURE));
 }
 
 int	check_not_num_with_space(void)
 {
-	char	*list[3];
-	int		result;
+	t_check_list_int	check;
 
-	list[0] = "012  1";
-	list[1] = "2P  	70";
-	list[2] = "\0";
-	result = check_list_integer(list);
-	if (result == EXIT_FAILURE)
+	check.list[0] = "012  1";
+	check.list[1] = "2P  	70";
+	check.list[2] = "\0";
+	check.result = check_list_integer(check.list);
+	if (check.result == EXIT_FAILURE)
 	{
-		printf("[check_list_integer_test.c, check_not_num_with_space]");
-		printf("- OK\n");
-		return (EXIT_SUCCESS);
+		return (print_status("check_list_integer_test.c",
+		"check_not_num_with_space", "OK", EXIT_SUCCESS));
 	}
-	printf("[check_list_integer_test.c, check_not_num_with_space]");
-	printf("- Ensure returns 1 if list containst other non-number char\n");
-	return (EXIT_FAILURE);
+	return (print_status("check_list_integer_test.c",
+	"check_not_num_with_space",
+	"Ensure returns 1 if list containst other non-number char", EXIT_FAILURE));
 }
