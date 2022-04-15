9a164f6deaa26df46fdbd7630dcba35ea20d79d3
Mitsu325 <patricia.mashiba@gmail.com>
2022-04-15 08:18:26 -0300

    test: exit safe in parser save function

--- src/tests/integration/parser_save_test_integration.c
+++ src/tests/integration/parser_save_test_integration.c
@@ -6,7 +6,7 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 06:38:59 by pmitsuko          #+#    #+#             */
-/*   Updated: 2022/04/15 07:50:58 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2022/04/15 08:16:34 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,11 +53,35 @@ typedef struct s_parser_save
 // 	return (SUCCESS);
 // }
 
+static int	test_multiple_number(void)
+{
+	t_parser_save check;
+
+	check.argv[0] = "./push_swap";
+	check.argv[1] = "142";
+	check.argv[2] = "2  -9";
+	check.argv[3] = "05";
+	check.argv[4] = "\0";
+	check.last = NULL;
+	check.result = parser_save(check.argv, &check.last);
+	// ft_putlst_fd(check.last, 1);
+	ft_lstclear(&check.last);
+	if (check.result == SUCCESS)
+	{
+		return (print_status("parser_save", "test_multiple_number", "OK",
+		SUCCESS));
+	}
+	return (print_status("parser_save", "test_multiple_number",
+	"Ensure returns 0 if save numbers", FAILURE));
+}
+
 int	parser_save_test_i(void)
 {
 	// if (test_empty_string())
 	// 	return (FAILURE);
 	// if (test_one_number())
 	// 	return (FAILURE);
+	if (test_multiple_number())
+		return (FAILURE);
 	return (SUCCESS);
 }
