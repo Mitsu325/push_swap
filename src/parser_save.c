a353878bd7c95b076e5c25342ffad00c742b5b41
Mitsu325 <patricia.mashiba@gmail.com>
2022-04-15 11:44:53 -0300

    feat: ensure put Error if argv are not int

--- src/parser_save.c
+++ src/parser_save.c
@@ -6,7 +6,7 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/14 06:36:51 by pmitsuko          #+#    #+#             */
-/*   Updated: 2022/04/15 08:27:24 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2022/04/15 11:42:50 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,8 @@
 
 void	exit_safe(t_list **last, int status)
 {
+	if (status == FAILURE)
+		ft_putstr_fd("Error\n", 1);
 	ft_lstclear(last);
 	exit(status);
 }
@@ -24,7 +26,7 @@ int	parser_save(char **argv, t_list **last)
 	int	i;
 
 	if (check_list_integer(++argv))
-		return (FAILURE);
+		exit_safe(last, FAILURE);
 	i = 0;
 	while (*(argv + i) && **(argv + i))
 	{
