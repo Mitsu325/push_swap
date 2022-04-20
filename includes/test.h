44b003d118fb9dc598fbd3054c939666a43ea3ed
Mitsu325 <patricia.mashiba@gmail.com>
2022-04-20 05:43:10 -0300

    test: rotate stack

--- includes/test.h
+++ includes/test.h
@@ -6,7 +6,7 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 05:24:28 by pmitsuko          #+#    #+#             */
-/*   Updated: 2022/04/18 06:32:53 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2022/04/20 05:13:28 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ int	check_sort_test(void);
 int	operation_test(void);
 int	swap_test(void);
 int	push_test(void);
+int	rotate_test(void);
 
 // Test Integration
 int	parser_save_test_i(void);
--- src/tests/operation/operation_test.c
+++ src/tests/operation/operation_test.c
@@ -6,7 +6,7 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/16 17:24:37 by pmitsuko          #+#    #+#             */
-/*   Updated: 2022/04/18 06:32:45 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2022/04/20 05:13:46 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	operation_test(void)
 	if (swap_test())
 		return (FAILURE);
 	if (push_test())
+		return (FAILURE);
+	if (rotate_test())
 		return (FAILURE);
 	return (SUCCESS);
 }
