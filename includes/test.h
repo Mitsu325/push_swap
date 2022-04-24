bc2e2bcfce05eb00c01d2cc1227002850d609356
Mitsu325 <patricia.mashiba@gmail.com>
2022-04-24 18:41:28 -0300

    test: sort up to five numbers

--- includes/test.h
+++ includes/test.h
@@ -6,7 +6,7 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/06 05:24:28 by pmitsuko          #+#    #+#             */
-/*   Updated: 2022/04/22 05:03:53 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2022/04/22 06:30:36 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ int	reverse_rotate_test(void);
 int	sort_test(void);
 int	two_number_test(void);
 int	three_number_test(void);
+int	five_number_test(void);
 
 // Test Integration
 int	parser_save_test_i(void);
--- src/tests/sort/sort_test.c
+++ src/tests/sort/sort_test.c
@@ -6,7 +6,7 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 08:19:20 by pmitsuko          #+#    #+#             */
-/*   Updated: 2022/04/22 05:03:36 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2022/04/22 08:39:03 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	sort_test(void)
 	if (two_number_test())
 		return (FAILURE);
 	if (three_number_test())
+		return (FAILURE);
+	if (five_number_test())
 		return (FAILURE);
 	return (SUCCESS);
 }
