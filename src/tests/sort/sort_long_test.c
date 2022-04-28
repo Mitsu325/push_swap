e521e4757b3cefd9d8ceddcc5deb07a83b3029eb
Mitsu325 <patricia.mashiba@gmail.com>
2022-04-28 06:23:12 -0300

    test: quicksort array

--- src/tests/sort/sort_long_test.c
+++ src/tests/sort/sort_long_test.c
@@ -6,7 +6,7 @@
 /*   By: pmitsuko <pmitsuko@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/20 08:20:33 by pmitsuko          #+#    #+#             */
-/*   Updated: 2022/04/27 08:02:57 by pmitsuko         ###   ########.fr       */
+/*   Updated: 2022/04/28 06:10:46 by pmitsuko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,7 +40,33 @@ static void	set_twenty_numbers(t_stack *stack)
 	ft_lstadd_back(&stack->last_a, -2);
 }
 
-static int	test_num_format_int(void)
+static int	check_sorted_array(int *num, int size)
+{
+	int	i;
+
+	i = 0;
+	while (i < (size - 1))
+	{
+		if (num[i] > num[i + 1])
+			return (FAILURE);
+		i++;
+	}
+	return (SUCCESS);
+}
+
+static void	put_arr(int *num, int size)
+{
+	int	i;
+
+	i = 0;
+	while (i < size)
+	{
+		printf("[%d]\n", num[i]);
+		i++;
+	}
+}
+
+static int	test_num_format_array(void)
 {
 	t_stack	stack;
 	int		*result;
@@ -51,18 +77,42 @@ static int	test_num_format_int(void)
 	{
 		free(result);
 		ft_lstclear(&stack.last_a);
-		return (print_status("sort_long_test.c", "test_num_format_int",
+		return (print_status("sort_long_test.c", "test_num_format_array",
 		"OK", SUCCESS));
 	}
 	free(result);
 	ft_lstclear(&stack.last_a);
-	return (print_status("sort_long_test.c", "test_num_format_int",
+	return (print_status("sort_long_test.c", "test_num_format_array",
 	"Ensure numbers save in array", FAILURE));
+}
+
+static int	test_sort_array(void)
+{
+	t_stack	stack;
+	int		*result;
+
+	set_twenty_numbers(&stack);
+	result = save_numbers_in_array(&stack);
+	quicksort(result, 0, stack.full_size - 1);
+	put_arr(result, stack.full_size);
+	if (check_sorted_array(result, stack.full_size) == SUCCESS)
+	{
+		free(result);
+		ft_lstclear(&stack.last_a);
+		return (print_status("sort_long_test.c", "test_sort_array",
+		"OK", SUCCESS));
+	}
+	free(result);
+	ft_lstclear(&stack.last_a);
+	return (print_status("sort_long_test.c", "test_sort_array",
+	"Ensure sorted array", FAILURE));
 }
 
 int	sort_long_test(void)
 {
-	if (test_num_format_int())
+	if (test_num_format_array())
+		return (FAILURE);
+	if (test_sort_array())
 		return (FAILURE);
 	return (SUCCESS);
 }
